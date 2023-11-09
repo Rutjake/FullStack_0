1000b045a62e6c51bd24e04550d6ad859e0bd3e7
Rutjake <jarnosanterimusic@gmail.com>
2023-11-09 17:18:42 +0200

    Simple Code

--- src/services/notes.tsx
+++ src/services/notes.tsx
@@ -3,18 +3,18 @@ const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
-    return request.then(response => {
-        return response.data
-    })
+    return request.then(response => response.data)
 }
 
 const create = newObject => {
-  return axios.post(baseUrl, newObject)
-}
-
-const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject)
-}
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+  }
+  
+  const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+  }
 
 export default { 
   getAll: getAll, 
